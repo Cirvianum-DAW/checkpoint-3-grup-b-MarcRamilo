54002f7d21d06bcdfa017351af98f75170db1609
alexllastanos <alex.llastanos@cirvianum.cat>
2024-04-30 17:06:38 +0200

    tercer commit

--- General/pokemon.js
+++ General/pokemon.js
@@ -14,6 +14,27 @@ try {
   }
 }
 
+async function requestHability(param){
+    try {
+        const response = await fetch(`https://pokeapi.co/api/v2/ability/${param}`);
+        if (!response.ok) {
+          throw new Error('Failed to fetch weather data');
+        }
+        const data = await response.json();
+        // console.log(data);
+        return data;
+      } catch (error) {
+        throw error;
+      }
+}
+async function habilitats(id){
+    try{
+        const habilitats = await requestHability(id);
+        return habilitats;
+    } catch(e){
+        throw e;
+    }
+}
 // request('ditto');
 const pokemonsSelected = [];
 
@@ -25,27 +46,52 @@ async function getInfo(info){
             id: data.id,
             name: data.name,
             height: data.height,
+            weight : data.weight,
             habilitats:data.abilities,
             sprites: data.sprites
         }
-       // console.log(objecte);
+        console.log(objecte);
 
         return objecte;
     } catch (e){
         throw e;
     }
 }
+async function getDescription(info){
+    try{
+        const data = await request(info);
+        return objecte;
+    } catch (e){
+        throw e;
+    }
+}
+
 document.addEventListener('DOMContentLoaded', async function (event)  {
     const pokemon = await getInfo('132');
-    const name = document.getElementById('name');
-    const height = document.getElementById('height');
-    const weight = document.getElementById('weight');
+    document.getElementById('name').innerHTML =('Nom:' + pokemon.name);
+     document.getElementById('height').innerHTML =('Heigth: ' + pokemon.height);
+     document.getElementById('weight').innerHTML =('Weigth: ' +pokemon.weight);
+    habilitats = pokemon.habilitats;
+    const hab = pokemon.habilitats.map((i) => i);
+    document.getElementById('sprite').src = pokemon.sprites.back_default; 
+
+    habilitats.forEach(element => {
+        const h4 = document.createElement('h4')
+        //document.querySelector('h4').innerHTML =(element.ability.name);
+        h4.textContent = element.ability.name;
+        const titol = document.querySelector('h4');
+        titol.appendChild(h4);
+
+        const description = document.createElement('p');
+        //document.querySelector('h4').innerHTML =(element.ability.name);
+        //description.textContent = element.ability;
+        //const descripcioHTML = document.getElementById('description');
+        //description.appendChild(descripcioHTML);
+    });
+    //console.log(habilitats)
+
+
     
-
-
-    console.log(id)
-    div.appendChild(id)
 })
 
 
-getInfo('131');
--- UF3/validacions.js
+++ UF3/validacions.js
@@ -2,10 +2,10 @@
 
 // Selecció dels inputs...
 const nameInput = document.querySelector('input[name="name"]');
-
+const cognomInput = document.querySelector('input[surname="surname"]');
+const mailInput = document.querySelector('input[mail="mail"]')
 // Si no saps com utilitzar aquestes funcions, pots fer ús de missatges per consol o situar
 // els errors allà on consideris lògic
-
 // Funció per eliminar missatges d'error existents
 function removeExistingError(input) {
   const existingError = input.nextElementSibling;
@@ -27,10 +27,53 @@ function displayError(input, message) {
 
 function validateName() {
   removeExistingError(nameInput);
-  // Validació del nom...
-
-  // Si no és vàlid...
-  displayError(nameInput, "El nom no és vàlid");
+  let nomRegex = /^[a-zA-ZÀ-ÿ\s]{3,50}$/;
+  return nomRegex.test(nameInput);
+}
+function validateSurname() {
+  removeExistingError(cognomInput);
+  let cognomRegex = /^[a-zA-ZÀ-ÿ\s]{3,50}$/;
+  return cognomRegex.test(cognomInput);
 }
 
+function validateMail() {
+  removeExistingError(mailInput);
+  displayError(mailInput, "El mail no és vàlid");
+  let mailRegex = /^[^@]+@[^@]+\.[^@]+$/;
+  return mailRegex.test(mailInput);
+}
 // addEventListeners...
+document
+  .getElementById('formulariId')
+  .addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    if(!validateName){
+      displayError(nameInput, "El nom no és vàlid");
+      console.error('Error');
+
+    }
+  })
+
+  document
+  .getElementById('formulariId')
+  .addEventListener('input', function (event) {
+    event.preventDefault();
+
+    if(!validateSurname){
+      displayError(cognomInput, "El cognom no és vàlid");
+      console.error('Error');
+    }
+  })
+
+  document
+  .getElementById('formulariId')
+  .addEventListener('input', function (event) {
+    event.preventDefault();
+
+    if(!validateMail){
+      displayError(cognomInput, "El mail no és vàlid");
+      console.error('Error');
+
+    }
+  })
