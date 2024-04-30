fd916d7b05776f833b5388da8a589909b55ec215
alexllastanos <alex.llastanos@cirvianum.cat>
2024-04-30 17:55:52 +0200

    quart commit

--- UF3/validacions.js
+++ UF3/validacions.js
@@ -1,9 +1,24 @@
 // A continuació et mostro algunes idees per poder començar a organitzar el teu codi.
 
+async function request() {
+  try {
+    const res = await fetch("./productes.js");
+    //const data = await res.json();
+    //console.log(res);
+    return res;
+  } catch (e) {
+    console.error('Error', e);
+  }
+}
+request();
 // Selecció dels inputs...
 const nameInput = document.querySelector('input[name="name"]');
-const cognomInput = document.querySelector('input[surname="surname"]');
-const mailInput = document.querySelector('input[mail="mail"]')
+const cognomInput = document.getElementById('surname');
+const mailInput = document.getElementById('email')
+const edatInput = document.getElementById('age');
+const meessageInput = document.getElementById('message');
+
+console.log(nameInput, cognomInput, mailInput,edatInput)
 // Si no saps com utilitzar aquestes funcions, pots fer ús de missatges per consol o situar
 // els errors allà on consideris lògic
 // Funció per eliminar missatges d'error existents
@@ -22,6 +37,28 @@ function displayError(input, message) {
   //https://developer.mozilla.org/es/docs/Web/API/Element/insertAdjacentElement
   input.insertAdjacentElement("afterend", error);
 }
+function validarDataNaixement() {
+
+
+  let avui = new Date();
+
+  let anyNaixement = 2024 - edatInput.value  ;
+
+
+  //let edat = avui.getFullYear() - dataNaixement;
+
+  if ( edatInput.value = null) 
+  {
+    displayError(edatInput, "El camp es requerit");
+  } else if(anyNaixement > 2007 && edatInput.value !=null ){
+    displayError(edatInput, "No es major d'edat");
+  } else{
+    return edatInput.value;
+
+  }
+    
+  } 
+
 
 // Cada validació...
 
@@ -38,10 +75,17 @@ function validateSurname() {
 
 function validateMail() {
   removeExistingError(mailInput);
-  displayError(mailInput, "El mail no és vàlid");
   let mailRegex = /^[^@]+@[^@]+\.[^@]+$/;
   return mailRegex.test(mailInput);
 }
+
+function validateMessage() {
+  removeExistingError(meessageInput);
+  let messageRegex = /^[a-zA-ZÀ-ÿ1-100\s]{10,200}$/;
+  return messageRegex.test(meessageInput);
+}
+
+
 // addEventListeners...
 document
   .getElementById('formulariId')
@@ -57,23 +101,53 @@ document
 
   document
   .getElementById('formulariId')
-  .addEventListener('input', function (event) {
+  .addEventListener('submit', function (event) {
     event.preventDefault();
 
     if(!validateSurname){
       displayError(cognomInput, "El cognom no és vàlid");
       console.error('Error');
     }
+
   })
 
   document
   .getElementById('formulariId')
-  .addEventListener('input', function (event) {
+  .addEventListener('submit', function (event) {
     event.preventDefault();
 
     if(!validateMail){
-      displayError(cognomInput, "El mail no és vàlid");
+      displayError(mailInput, "El mail no és vàlid");
       console.error('Error');
 
     }
+  })
+
+  document
+  .getElementById('formulariId')
+  .addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    if (!validarDataNaixement()) {
+      displayError(edatInput, "Error");
+    }
+  })
+
+  document
+  .getElementById('formulariId')
+  .addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    if (!validateMessage()) {
+      displayError(meessageInput, "Missatge no valid");
+    }
+  })
+
+
+  document
+  .getElementById('formulariId')
+  .addEventListener('submit', function (event) {
+    event.preventDefault();
+
+
   })
